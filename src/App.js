076d381eaf3fa5efcd96f676b6a819f008068a5b
Mikhael <116017125+mikhaelnice@users.noreply.github.com>
2023-05-08 13:32:13 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import NftComingSoon from "./pages/NftComingSoon";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <NftComingSoon />
+      <Footer />
+    </>
+  );
+}
+export default App;
